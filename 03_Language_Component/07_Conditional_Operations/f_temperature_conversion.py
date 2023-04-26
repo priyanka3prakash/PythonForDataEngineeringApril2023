60714fe771adade48af6902c43a5774522c70b54
priyanka3prakash <130571847+priyanka3prakash@users.noreply.github.com>
2023-04-26 15:40:31 +0000

    class12 - conditionals ops, structural pattern matching, loops -for & while

---
+++ 03_Language_Component/07_Conditional_Operations/f_temperature_conversion.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Purpose: Temperature Conversions
+    - celsius to fahrenheit , or vice versa
+
+
+user input : 23c
+output     : xF
+
+user input : 23F
+output     : xC
+
+23c, 23C, 23F, 23f
+23C, 23F
+23 C
+23C
+"""
+import sys
+
+temperature = input("Enter the temperature, followed by C or F:").upper().strip()
+
+if "C" in temperature:
+    celsius = float(temperature.strip("C"))
+    fahrenheit = (1.8 * celsius) + 32
+elif "F" in temperature:
+    fahrenheit = float(temperature.strip("F "))
+    celsius = (fahrenheit - 32) * 5.0 / 9.0
+else:
+    temperature = temperature.strip()
+    if temperature.isdigit():
+        # Default is celsius
+        celsius = float(temperature.strip("C "))
+        fahrenheit = (1.8 * celsius) + 32
+    else:
+        print("INVALID INPUT")
+        sys.exit(1)
+print(
+    f"""
+        celsius     : {round(celsius, 2)}
+        fahrenheit  : {round(fahrenheit, 2)}
+"""
+)
+
---
+++ 03_Language_Component/08_Structural_Pattern_Matching/a_vowel_char_check.py
@@ -0,0 +1,33 @@
+"""
+Purpose: To check largest of give two numbers
+"""
+
+letter = input("Enter any character:").strip()
+
+
+if letter in "aeiou":
+    print(f"{letter} is a LOWER VOWEL CHARACTER")
+elif letter in "AEIOU":
+    print(f"{letter} is a UPPER VOWEL CHARACTER")
+else:
+    print(f"{letter} may be a CONSONANT")
+
+
+# match letter:
+#     case (letter in "aeiou"):
+#         print(f"{letter} is a LOWER VOWEL CHARACTER")
+#     case (letter in "AEIOU"):
+#         print(f"{letter} is a UPPER VOWEL CHARACTER")
+#     case _:
+#         print(f"{letter} may be a CONSONANT")
+
+# # NOTE: in operator wont work in match condition check
+
+
+match letter:
+    case "a" | "e" | "i" | "o" | "u":
+        print(f"{letter} is a LOWER VOWEL CHARACTER")
+    case "A" | "E" | "I" | "O" | "U":
+        print(f"{letter} is a UPPER VOWEL CHARACTER")
+    case _:
+        print(f"{letter} may be a CONSONANT")
