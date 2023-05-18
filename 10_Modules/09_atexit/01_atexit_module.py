475b8f43c441c56cebdaf30194db11fa4e9c2c88
Priyanka <130571847+priyanka3prakash@users.noreply.github.com>
2023-05-18 15:27:55 +0000

    class28 - Modules - collections,atexit,random

---
+++ 10_Modules/09_atexit/01_atexit_module.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+"""
+Purpose: atexit
+    - Call functions when a program is closing down
+"""
+import atexit
+
+
+def hello():
+    print("Hello world")
+
+def add(n1, n2):
+    print("add - function")
+    return n1 + n2
+
+
+def all_done():
+    print("all_done()")
+
+
+print("Registering")
+atexit.register(all_done)
+print("Registered")
+
+
+if __name__ == "__main__":
+    hello()
+    # print(dir(atexit))
+    # all_done()
+
+    res = add(10, 20)
+    print(f"{res = }")
---
+++ 10_Modules/10_random/04_random_name_generator.py
@@ -0,0 +1,9 @@
+def random_name_generator(first, second, x):
+    pass
+
+
+
+first_names = ["Drew", "Mike", "Landon", "Jeremy", "Tyler", "Tom", "Avery"]
+last_names = ["Smith", "Jones", "Brighton", "Taylor"]
+names = random_name_generator(first_names, last_names, 5)
+print("\n".join(names))
