c6fc2a99c4db4af67b9c82dd21e02e0cb474ff7b
Priyanka <130571847+priyanka3prakash@users.noreply.github.com>
2023-05-18 21:29:25 +0000

    class20 - Modules - collections, atexit, random

--- 10_Modules/10_random/04_random_name_generator.py
+++ 10_Modules/10_random/04_random_name_generator.py
@@ -1,5 +1,24 @@
-def random_name_generator(first, second, x):
-    pass
+import random
+
+
+def random_name_generator(first, second, count):
+    _names = []
+    for _ in range(count):
+        fst_name = random.choice(first)
+        lst_name = random.choice(second)
+        name  = f"{fst_name} {lst_name}"
+        _names.append(name)
+    return _names
+
+
+def random_name_generator(first, second, count):
+    _names = set()
+    while len(_names) < count:
+        fst_name = random.choice(first)
+        lst_name = random.choice(second)
+        name  = f"{fst_name} {lst_name}"
+        _names.add(name)
+    return _names
 
 
 
@@ -7,3 +26,9 @@ first_names = ["Drew", "Mike", "Landon", "Jeremy", "Tyler", "Tom", "Avery"]
 last_names = ["Smith", "Jones", "Brighton", "Taylor"]
 names = random_name_generator(first_names, last_names, 5)
 print("\n".join(names))
+
+
+# Assignment:
+# In runtime, take the gender name as input, and generate random name.
+# HINT: Take a dataset of female first names, and another with male first names
+# one dataset with last names
