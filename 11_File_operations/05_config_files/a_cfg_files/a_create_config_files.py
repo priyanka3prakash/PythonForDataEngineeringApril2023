9e4622565e9bccd2182e0fb31138d70784375d59
Priyanka <130571847+priyanka3prakash@users.noreply.github.com>
2023-05-26 15:32:05 +0000

    class34 - zip & config files

---
+++ 11_File_operations/05_config_files/a_cfg_files/a_create_config_files.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+"""
+Purpose: Reading (Parsing) config files
+"""
+
+# python 2.x - ConfigParser
+# python 3.x - configparser
+
+import configparser
+
+# print(dir(configparser))
+
+config = configparser.RawConfigParser()
+
+
+config.add_section("Section1")
+config.set("Section1", "an_int", "15")
+config.set("Section1", "a_bool", "true")
+config.set("Section1", "a_float", "3.1415")
+
+config.add_section("Section2")
+config.set("Section2", "baz", "fun")
+config.set("Section2", "bar", "Python")
+config.set("Section2", "foo", "%(bar)s is %(baz)s!")
+
+
+# Writing our configuration file to 'example.cfg'
+with open("example.cfg", "w", encoding="utf-8") as configfile:
+    config.write(configfile)
